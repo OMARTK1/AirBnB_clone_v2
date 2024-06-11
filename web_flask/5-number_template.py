cc089285706c76a63067a32aba92a872592d33b6
OMARTK1 <elyazidi.omar@gmail.com>
2024-06-11 16:01:50 +0100

    webflask/temlates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-
-"""The web application listening on 0.0.0.0, port 5000
-Routes:
-    - /: display “Hello HBNB!”
-    - /hbnb: display “HBNB”
-    - /c/<text>: display “C ” followed by value of the text variable
-        (replace underscore _ symbols with a space )
-    - `/python/<text>`: display “Python ”, followed by the value of the text
-        variable (replace underscore _ symbols with a space )
-    - /number/<n>: display “`n` is a number” only if `n` is an integer
-    - /number_template/<n>: displaying a HTML page only if n is an integer:
-        - H1 tag: “Number: n” inside the tag BODY
-And use the option strict_slashes=False in your route definition"""
+"""Flask web application"""
 
 from flask import Flask, render_template
 
@@ -20,39 +8,39 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """calling through the / route"""
+    """Displays 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """calling through the /hbnb route"""
+    """Displays 'HBNB'."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """calling through the /c/<text> route"""
-    return 'C ' + text.replace('_', ' ')
+    """Displays 'C ' followed by the value of the text variable."""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text):
-    """calling through the /python/<text> route"""
+    """Displays 'Python ' followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
-    """displaying "n is a number" if n is indeed an integer"""
-    return '{:d} is a number'.format(n)
+    """Displays 'n is a number' if n is an integer."""
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """displaying an HTML page only if n is an integer"""
-    return render_template('5-number_template.html', n=n)
+    """Displays an HTML page only if n is an integer."""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
